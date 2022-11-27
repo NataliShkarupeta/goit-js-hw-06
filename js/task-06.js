e7a281d112affe49169be21b3d2fabef6ceb58a5
Nataliia Shkarupeta <natalinardi.kh@gmail.com>
2022-11-27 09:30:45 +0200

    fix task6

--- js/task-06.js
+++ js/task-06.js
@@ -3,12 +3,14 @@ console.log(inputEl);
 
 inputEl.addEventListener('blur',onInputBlur)
     function onInputBlur(event){
-      if (event.currentTarget.value.length !== 6){
+   if (event.currentTarget.value.length !== 6){
+        if(event.currentTarget.classList.contains('valid')) {
+           event.currentTarget.classList.remove('valid')
+        }
         event.currentTarget.classList.add('invalid')
     } else
-      event.currentTarget.classList.add('valid')
+        if(event.currentTarget.classList.contains('invalid')) {
+            event.currentTarget.classList.remove('invalid')
+        }
+        event.currentTarget.classList.add('valid')
     }
-
-
-
-
